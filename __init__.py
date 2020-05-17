3394a6402e4a1165d16aa9c2654c40bad8b20498
anurag95kumar <31229771+anurag95kumar@users.noreply.github.com>
2020-05-17 12:42:32 +0530

    Update __init__.py
    Please feel free to generate and use your own Secret Key.
    Creating database site.db using SQLAlchemy

--- __init__.py
+++ __init__.py
@@ -4,8 +4,8 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'  
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'    #Creating database site.db using SQLAlchemy
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
